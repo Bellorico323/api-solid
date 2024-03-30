ad6ba9f1f9a5e5400ceb6be42edb2c16378555c7
Bellorico323 <mabo.orico@gmail.com>
2024-03-30 16:14:17 -0300

    CI Fixed

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -25,7 +25,7 @@ describe('Validate Check-in Use Case', () => {
     })
 
     const { checkIn } = await sut.execute({
-      checkInID: createdCheckIn.id,
+      checkInId: createdCheckIn.id,
     })
 
     expect(checkIn.validated_at).toEqual(expect.any(Date))
@@ -35,7 +35,7 @@ describe('Validate Check-in Use Case', () => {
   it('should not be able to validate an inexistent check-in', async () => {
     await expect(() =>
       sut.execute({
-        checkInID: 'inexistent-check-in-id',
+        checkInId: 'inexistent-check-in-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
@@ -54,7 +54,7 @@ describe('Validate Check-in Use Case', () => {
 
     await expect(() =>
       sut.execute({
-        checkInID: createdCheckIn.id,
+        checkInId: createdCheckIn.id,
       }),
     ).rejects.toBeInstanceOf(Error)
   })
